13c6272acf10a28ffee2beafed92bb314f00d0b5
Gandi <jakub.wasielak@yougov.com>
2017-05-02 07:35:01 +0200

    Port from os

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 import requests
 import requests_cache
 
@@ -16,4 +18,5 @@ def home(path):
     return jsonify(response_dict)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
