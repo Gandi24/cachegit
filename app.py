72fb450366fc789ac2d71950e968d3378e2ecbe9
Gandi <jakub.wasielak@yougov.com>
2017-05-02 07:15:12 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import requests
+import requests_cache
+
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+requests_cache.install_cache('github_cache', backend='sqlite', expire_after=60*60*24)
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def home(path):
+    url = "https://api.github.com/{0}".format(path)
+    response_dict = requests.get(url).json()
+    return jsonify(response_dict)
+
+if __name__ == '__main__':
+    app.run()
